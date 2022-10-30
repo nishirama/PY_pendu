9ab133ef2e431cfc765ae424a7785ed832b44ec3
Dream Maker <67145585+DR34M-M4K3R@users.noreply.github.com>
2022-10-30 12:26:26 +0100

    Update main.py
    Optiimisation de la création des touches de clavier

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from tkinter import *
 
 # https://www.python-gui-builder.com/
 
-def btnClickFunction():
-    print('clicked')
-
+class main:
 
 root = Tk()
 
@@ -16,57 +14,37 @@ root.configure(background='#595959')
 root.title('Py Pendu')
 
 # Début de création de touches de clavier
-Button(root, text='A', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=112, y=302)
 
-Button(root, text='B', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=162, y=302)
+def letterClicked(letter):
+    print(letter)
 
-Button(root, text='C', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=212, y=302)
 
-Button(root, text='D', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=262, y=302)
+al = "abcdefghij"
 
-Button(root, text='E', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=312, y=302)
+pha = "klmnopqrst"
 
-Button(root, text='F', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=362, y=302)
+bet = "uvwxyz"
 
-Button(root, text='G', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=412, y=302)
 
-Button(root, text='H', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=462, y=302)
+xPosition = 62
+for letter in al:
+    b = Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l))
+    b.place(x=(xPosition+50), y=302)
+    b.bind('<Return>', lambda event : print("test"))
+    print("la lettre ",letter,"est placée en position ", xPosition)
+    xPosition = xPosition+50
 
-Button(root, text='I', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=512, y=302)
+xPosition = 62
+for letter in pha:
+    Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l)).place(x=(xPosition+50), y=352)
+    print("la lettre ",letter,"est placée en position ", xPosition)
+    xPosition = xPosition+50
 
-Button(root, text='J', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=552, y=302)
-
-Button(root, text='K', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=112, y=352)
-
-Button(root, text='L', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=162, y=352)
-
-Button(root, text='M', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=212, y=352)
-
-Button(root, text='N', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=262, y=352)
-
-Button(root, text='O', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=312, y=352)
-
-Button(root, text='P', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=362, y=352)
-
-Button(root, text='Q', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=412, y=352)
-
-Button(root, text='R', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=462, y=352)
-
-Button(root, text='S', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=512, y=352)
-
-Button(root, text='T', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=562, y=352)
-
-Button(root, text='U', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=112, y=402)
-
-Button(root, text='V', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=162, y=402)
-
-Button(root, text='W', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=212, y=402)
-
-Button(root, text='X', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=262, y=402)
-
-Button(root, text='Y', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=312, y=402)
-
-Button(root, text='Z', bg='#A1A1A1', font=('verdana', 12, 'normal'), command=btnClickFunction).place(x=362, y=402)
+xPosition = 62
+for letter in bet:
+    Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l)).place(x=(xPosition+50), y=402)
+    print("la lettre ",letter,"est placée en position ", xPosition)
+    xPosition = xPosition+50
 # Fin de la création des touches de clavier
 
 # Création de l'image
