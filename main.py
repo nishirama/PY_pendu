08651b16278274c6bc97c6694c6b5023c3155637
Dream Maker <67145585+DR34M-M4K3R@users.noreply.github.com>
2022-11-16 11:59:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,66 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+import random
+
 
 
 # https://www.python-gui-builder.com/
 
-#class main:
+def motHasard():
+"""
 
+
+"""
+
+
+# Début de la création fenetre
 root = Tk()
-
 root.geometry('665x493')
-root.configure(background='#595959')
+root.configure(background='#242424')
 root.title('Py Pendu')
+root.iconbitmap('icone.ico')
+
 
 # Début de création de touches de clavier
-
 def letterClicked(letter):
     print(letter)
 
-
+# On divise l'alphabet en 3, pour faire les trois lignes
 al = "abcdefghij"
 
 pha = "klmnopqrst"
 
 bet = "uvwxyz"
 
-
 xPosition = 62
 for letter in al:
     b = Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l))
     b.place(x=(xPosition+50), y=302)
-    b.bind('<Return>', lambda event : print("test"))
-    print("la lettre ",letter,"est placée en position ", xPosition)
+    #Pour associer une touche du clavier du PC au bouton (marche pas)
+    #root.bind(letter, lambda event : print(l))
     xPosition = xPosition+50
 
 xPosition = 62
 for letter in pha:
     Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l)).place(x=(xPosition+50), y=352)
-    print("la lettre ",letter,"est placée en position ", xPosition)
     xPosition = xPosition+50
 
 xPosition = 62
 for letter in bet:
     Button(root, text=letter, bg='#A1A1A1', font=('verdana', 12, 'normal'), command=lambda l = letter : letterClicked(l)).place(x=(xPosition+50), y=402)
-    print("la lettre ",letter,"est placée en position ", xPosition)
     xPosition = xPosition+50
-# Fin de la création des touches de clavier
+
+#-- Fin de la création des touches de clavier --
+
 
 # Création de l'image
 image = Canvas(root, height=250, width=250)
-picture_file = PhotoImage(file='src/pendu_tour/frame1.png') # https://imageresizer.com/
+picture_file = PhotoImage(file='src/pendu_original/frame1.png') # https://imageresizer.com/
 image.create_image(250, 0, anchor=NE, image=picture_file)
 image.place(x=112, y=32)
+
+
+
 
 root.mainloop()
